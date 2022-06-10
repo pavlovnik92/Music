d3239ac14a665c9702b6667533eb38c7645b53a2
AliceRomanova <aliceromanova2004@mail.ru>
2022-06-10 16:29:01 +0300

    Initial Commit

---
+++ Music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Alice Romanova on 10.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
