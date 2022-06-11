d891a32707e60504982dca038ef75abbb67136bb
AliceRomanova <aliceromanova2004@mail.ru>
2022-06-11 13:44:59 +0300

    Реализвала класс NetworkService и проверила прихожящие данные

--- Music/SceneDelegate.swift
+++ Music/SceneDelegate.swift
@@ -19,7 +19,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = Music()
+        
+        let navigationControler = UINavigationController(rootViewController: Music())
+        
+        window?.rootViewController = navigationControler
         window?.makeKeyAndVisible()
     }
 
--- Music/Servises/NetworkService.swift
+++ Music/Servises/NetworkService.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+
+class NetworkService {
+    
+    func createRequest(request: String?, completion: @escaping (Data?, Error?) -> Void) {
+        
+        let parameters = prepareParameters(request: request)
+        let url = setupURL(parameters: parameters)
+
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "get"
+        
+        let task = createDataTask(request: request, completion: completion)
+        task.resume()
+    }
+    
+    private func prepareParameters(request: String?) -> [String: String] {
+        var parameters = [String: String]()
+        
+        parameters["term"] = request
+        parameters["media"] = "music"
+        
+        return parameters
+    }
+    
+    private func setupURL(parameters: [String: String]) -> URL {
+        var components = URLComponents()
+        
+        components.scheme = "https"
+        components.host = "itunes.apple.com"
+        components.path = "/search"
+        components.queryItems = parameters.map{ URLQueryItem(name: $0, value: $1) }
+        
+        return components.url!
+    }
+    
+    private func createDataTask(request: URLRequest, completion: @escaping (Data?, Error?) -> Void) -> URLSessionDataTask {
+        return URLSession.shared.dataTask(with: request) { data, _, error in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        }
+    }
+}
