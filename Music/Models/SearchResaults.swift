7d14669d70fa2054b53ebd343a6b5ca5d97630e7
AliceRomanova <aliceromanova2004@mail.ru>
2022-06-11 12:49:37 +0300

    Реализовала модель SearchResults

--- Music/Models/SearchResaults.swift
+++ Music/Models/SearchResaults.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+
+struct SearchResaults: Decodable {
+    
+    let resultCount: Int
+    let results: [SongParameters]
+}
+
+struct SongParameters: Decodable {
+    
+    let artistName: String
+    let collectionName: String?
+    let trackName: String
+    let artworkUrl60: String?
+}
