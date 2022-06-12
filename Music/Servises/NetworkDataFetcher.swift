6215d168d702e55e64d15499536da30561982d99
AliceRomanova <aliceromanova2004@mail.ru>
2022-06-12 19:41:39 +0300

    Реализовала класс NetworkDataFetcher

--- Music/Servises/NetworkDataFetcher.swift
+++ Music/Servises/NetworkDataFetcher.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+
+class NetworkDataFetcher {
+    
+    private var networkService = NetworkService()
+    
+    func fetchMusic(request: String?, completion: @escaping (SearchResaults?) -> Void) {
+        
+        networkService.createRequest(request: request) { data, error in
+            if let error = error {
+                print("Error received requesting data: \(error.localizedDescription)")
+                completion(nil)
+            }
+        }
+    }
+    
+    private func decodeJSON<T: Decodable>(type: T.Type, data: Data?) -> T? {
+        let decoder = JSONDecoder()
+        
+        guard let data = data else { return nil }
+        
+        do {
+            let objects = try decoder.decode(type.self, from: data)
+            return objects
+        } catch {
+           print("Faild to decode JSON")
+            return nil
+        }
+    }
+}
