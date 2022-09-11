08e0a732f66a0cf13c0c344d7f45be626c879551
AliceRomanova <aliceromanova2004@mail.ru>
2022-09-11 12:47:05 +0300

    Устранила баг, связанный с перетаскиванием ползунка песни. Нстроила ползунок громкости

--- Music/TrackModule/TrackViewController.swift
+++ Music/TrackModule/TrackViewController.swift
@@ -5,8 +5,8 @@
 //  Created by Alice Romanova on 31.08.2022.
 //
 
-import UIKit
 import AVKit
+import MediaPlayer
 
 protocol TrackDiasplayLogic: AnyObject {
     func displayData(data: TrackModels.ModelType.ViewModel.ViewModelType)
@@ -36,7 +36,7 @@ final class TrackViewController: UIViewController {
     private let backButton = UIButton()
     private let pauseButton = UIButton()
     private let forwardButton = UIButton()
-    private let addingButton = UIButton()
+
      
     //MARK: - Track components
     private var currentTrack: String!
@@ -50,8 +50,6 @@ final class TrackViewController: UIViewController {
 
         view.backgroundColor = .systemBackground
 //        navigationItem.hidesBackButton = true
-        
-        setupPlayer()
 
         setupAlbumImageView()
         setupConstraintsForAlbumImageView()
@@ -81,6 +79,13 @@ final class TrackViewController: UIViewController {
         setupConstraintsForVolumeSlider()
     }
     
+    //MARK: - viewDidAppear
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        setupPlayer()
+    }
+    
     //MARK: - Animations
     
     private func setPauseAnimation() {
@@ -108,14 +113,17 @@ final class TrackViewController: UIViewController {
     //MARK: - setupPlayer
     
     private func setupPlayer() {
-        player.automaticallyWaitsToMinimizeStalling = false
         let url = URL(string: currentTrack)
-        
         let item = AVPlayerItem(url: url!)
+
+        player.automaticallyWaitsToMinimizeStalling = false
         player.replaceCurrentItem(with: item)
         player.play()
         
-        player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1000), queue: .main) { [weak self] time in
+        player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 10), queue: .main) {
+            [weak self] time in
+
+            //почему-то если тянуть ползунок и отмотать на десятую секунду, то показывает 00:010, а когда он сам идет такого нет
             if time.seconds < 10 {
                 self?.timeLabel.text = "00:0\(NSString(format: "%.0f", time.seconds))"
             } else {
@@ -194,8 +202,9 @@ final class TrackViewController: UIViewController {
     
     private func setupTrackSlider() {
         trackSlider.maximumValue = Float(player.currentItem?.asset.duration.seconds ?? 30)
-        
         trackSlider.addTarget(self, action: #selector(rearrange), for: .valueChanged)
+        trackSlider.minimumTrackTintColor = .systemGray2
+      
     }
     
     private func setupConstraintsForTrackSlider() {
@@ -206,7 +215,6 @@ final class TrackViewController: UIViewController {
         trackSlider.topAnchor.constraint(equalTo: artistNameLabel.bottomAnchor, constant: 30).isActive = true
         trackSlider.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         trackSlider.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        trackSlider.heightAnchor.constraint(equalToConstant: 1).isActive = true
     }
     
     //MARK: - setupTimeLabel
@@ -222,7 +230,7 @@ final class TrackViewController: UIViewController {
         
         view.addSubview(timeLabel)
         
-        timeLabel.topAnchor.constraint(equalTo: trackSlider.bottomAnchor, constant: 17).isActive = true
+        timeLabel.topAnchor.constraint(equalTo: trackSlider.bottomAnchor, constant: 10).isActive = true
         timeLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 23).isActive = true
         timeLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
         timeLabel.heightAnchor.constraint(equalToConstant: 15).isActive = true
@@ -240,7 +248,7 @@ final class TrackViewController: UIViewController {
         view.addSubview(backButton)
         
         backButton.transform = backButton.transform.rotated(by: .pi)
-        backButton.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 65).isActive = true
+        backButton.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 45).isActive = true
         backButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50).isActive = true
         backButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
         backButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
@@ -258,7 +266,7 @@ final class TrackViewController: UIViewController {
         
         view.addSubview(pauseButton)
         
-        pauseButton.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 60).isActive = true
+        pauseButton.topAnchor.constraint(equalTo: backButton.topAnchor).isActive = true
         pauseButton.leftAnchor.constraint(equalTo: backButton.rightAnchor, constant: 50).isActive = true
         pauseButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
         pauseButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
@@ -275,34 +283,21 @@ final class TrackViewController: UIViewController {
         
         view.addSubview(forwardButton)
         
-        forwardButton.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 65).isActive = true
+        forwardButton.topAnchor.constraint(equalTo: pauseButton.topAnchor, constant: 3).isActive = true
         forwardButton.leftAnchor.constraint(equalTo: pauseButton.rightAnchor, constant: 50).isActive = true
         forwardButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
         forwardButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
     
-    //MARK: - setupAddingButton
-    
-    private func setupAddingButton() {
-        
-    }
-    
-    private func setupConstraintsForAddingButton() {
-        artistNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(artistNameLabel)
-        
-        artistNameLabel.topAnchor.constraint(equalTo: trackNameLabel.bottomAnchor, constant: 5).isActive = true
-        artistNameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        artistNameLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        artistNameLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
-    }
-    
     //MARK: - setupVolumeSlider
     
     private func setupVolumeSlider() {
+        volumeSlider.value = player.volume
+        
         volumeSlider.maximumValueImage = UIImage(named: "max")
         volumeSlider.minimumValueImage = UIImage(named: "min")
+        
+        volumeSlider.addTarget(self, action: #selector(changeVolume), for: .valueChanged)
     }
     
     private func setupConstraintsForVolumeSlider() {
@@ -315,23 +310,31 @@ final class TrackViewController: UIViewController {
         volumeSlider.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -50).isActive = true
     }
     
+    //MARK: - @objc method "stop"
+    
     @objc private func stop() {
         if player.timeControlStatus == .paused {
-            setPauseAnimation()
+            setPlayAnimation()
             player.play()
             pauseButton.setImage(UIImage(named: "pause"), for: .normal)
         } else {
             player.pause()
-            setPlayAnimation()
+            setPauseAnimation()
             pauseButton.setImage(UIImage(named: "play"), for: .normal)
         }
     }
     
+    //MARK: - @objc method "rearrange"
+    
     @objc private func rearrange() {
-   
-        player.seek(to: CMTime(seconds: Double(trackSlider.value), preferredTimescale: 1000))
+        player.seek(to: CMTime(seconds: Double(trackSlider.value), preferredTimescale: 10))
     }
-
+    
+    //MARK: - @objc method "changeVolume"
+    
+    @objc private func changeVolume() {
+        player.volume = volumeSlider.value
+    }
 }
 
 
